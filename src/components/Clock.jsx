6a6885ab7ca433afe3190355eab0b8089f8eb3fb
Hichem-BEN-AYED <benayedhicham@gmail.com>
2025-01-23 16:52:09 +0100

    Fin

--- src/components/Clock.jsx
+++ src/components/Clock.jsx
@@ -1,14 +1,22 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const Clock = () => {
     const [time, setTime] = useState(new Date());
+
+    useEffect (() => {
+        const intervalID = setInterval(() => {
+            setTime(new Date())
+        }, 1000)
+        return () => clearInterval(intervalID);
+    }, []);
+
     const hours = time.getHours();
     const minutes = time.getMinutes();
     const seconds = time.getSeconds();
     return (
         <div>
             <p>
-                Horloge : {hours} : {minutes} : {seconds}
+                Horloge : {hours} : {minutes < 10 ? "0" + minutes : minutes} : {seconds < 10 ? "0" + seconds : seconds}
             </p>
         </div>
     );
---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,10 @@
+
+const NotFound = () => {
+    return (
+        <div style={{textAlign : 'center', marginTop:50 }}>
+            <img src="./img/erreur.jpg" alt="Erreur404" width={500} />
+        </div>
+    );
+};
+
+export default NotFound;
